2669253f364fd6931a9f1281505b51896b1ecfd4
mparakouu <geobarakou7@gmail.com>
2024-05-10 13:20:24 +0300

    Ερωτημα 1.1 1,2 οκ

--- E1_1.py
+++ E1_1.py
@@ -1,13 +1,13 @@
 import cv2
 import numpy as np
 
-# Διαβάζουμε την εικόνα
+# διαβάζουμε την εικόνα
 leaf_image = cv2.imread('leaf.jpg')
 
-# Μετατρέπουμε την εικόνα σε grayscale
+# μετατρέπουμε την εικόνα σε grayscale
 grayscale_image = cv2.cvtColor(leaf_image, cv2.COLOR_BGR2GRAY)
 
-# Κατωφλιώνουμε με τιμή κατωφλίου τ = 220
+# κατωφλιώνουμε με τιμή κατωφλίου τ = 220
 _, thresholded_image = cv2.threshold(grayscale_image, 220, 255, cv2.THRESH_BINARY)
 
 # αρχική εικόνα leaf_image
@@ -19,7 +19,7 @@ cv2.imshow('Grayscale_image', grayscale_image)
 # δυαδική image show 
 cv2.imshow('Thresholded_image', thresholded_image)
 
-# Αντίστροφη δυαδική μετατροπή για να κάνουμε το φύλλο μαύρο και το φόντο λευκό
+# σε δυαδική 
 thresholded_image = cv2.bitwise_not(thresholded_image)
 
 # moore boundary tracing algorithm
@@ -28,23 +28,23 @@ def moore_boundary_algorithm(image):
     boundary = boundaries[0] if boundaries else None
     return boundary
 
-# Εξάγουμε το περίγραμμα του φύλλου από τη δυαδική εικόνα
+# εξάγουμε το περίγραμμα του φύλλου από τη δυαδική εικόνα
 leaf_with_pink_boundary = moore_boundary_algorithm(thresholded_image)
 
 # χρώμα για το περίγραμμα --> ροζ
 color = (255, 0, 255)  
 
-# Υπολογισμός περιγραφέων Fourier του περιγράμματος
+# υπολογισμός περιγραφέων Fourier του περιγράμματος
 fourier_descriptors = cv2.dft(leaf_with_pink_boundary.astype(np.float32), flags=cv2.DFT_COMPLEX_OUTPUT)
 
-# Εκτύπωση των περιγραφέων Fourier
+# περιγραφείς Fourier του boundary
 print("Περιγραφείς Fourier του περιγράμματος:")
 print(fourier_descriptors)
 
-# Σχειδάζουμε το ροζ περίγραμμα πάνω στην leaf_image
+# σχειδάζουμε το ροζ περίγραμμα πάνω στην leaf_image
 cv2.drawContours(leaf_image, [leaf_with_pink_boundary], -1, color, thickness=2)
 
-# Εμφάνιση leaf_image με ροζ περίγραμμα
+# εμφάνιση leaf_image με ροζ περίγραμμα
 cv2.imshow('Moore boundary tracing algorithm with pink boundary', leaf_image)
 
 cv2.waitKey(0)

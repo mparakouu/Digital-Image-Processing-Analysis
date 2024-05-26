ec0b053fe7f9f1ddaeb6f613d9e98bc3442145d3
mparakouu <geobarakou7@gmail.com>
2024-05-26 21:36:46 +0300

    E12 half ok

--- E12.py
+++ E12.py
@@ -1,24 +1,33 @@
 import cv2
 import numpy as np
 
-# Φόρτωση της εικόνας
-image = cv2.imread('lenna.jpg', cv2.IMREAD_GRAYSCALE)
+# lenna.jpg
+lenna_image = cv2.imread('lenna.jpg', cv2.IMREAD_GRAYSCALE)
 
-# Υπολογισμός διαστάσεων εικόνας
-height, width = image.shape
+height, width = lenna_image.shape
+block_size = 32 # block διαστάσεων 32x32 
 
-# Δημιουργία μιας κενής εικόνας για την αποθήκευση των αποτελεσμάτων
-compressed_image = np.zeros((height, width), dtype=np.float32)
+# DCT για κάθε block
+dct_blocks = [cv2.dct(np.float32(lenna_image[y:y+block_size, x:x+block_size])) 
 
-# Εφαρμογή μετασχηματισμού DCT σε κάθε μη επικαλυπτόμενη περιοχή 32x32
-for i in range(0, height, 32):
-    for j in range(0, width, 32):
-        # Περιοχή 32x32
-        block = image[i:i+32, j:j+32]
-        # Μετασχηματισμός DCT
-        dct_block = cv2.dct(np.float32(block))
-        # Αποθήκευση του μετασχηματισμένου block στην εικόνα συμπίεσης
-        compressed_image[i:i+32, j:j+32] = dct_block
+for y in range(0, height, block_size) for x in range(0, width, block_size) 
+if lenna_image[y:y+block_size, x:x+block_size].shape == (block_size, block_size)]
 
-# Αποθήκευση της συμπιεσμένης εικόνας
-cv2.imwrite('compressed_lenna.jpg', compressed_image)
+# για κάθε block στο dct_blocks --> εκτύπωση συντελεστών
+for i, block in enumerate(dct_blocks):
+    print(f"Οι συντελεστές του block {i} είναι:")
+    print(block)
+
+# block σε κάθε διάσταση 
+number_blocks_x = int(width / 32)
+number_blocks_y = int(height / 32)
+# όλα τα block σε μία εικόνα
+rows_blocks = [np.hstack(dct_blocks[i*number_blocks_x  : (i+1)*number_blocks_x ]) for i in range(number_blocks_y)] # βάζει τα block στην ίδια γραμμή 
+merged_blocks = np.vstack(rows_blocks) # βάζει τα block κατακόρυφα 
+final_blocks_image = np.uint8(merged_blocks) # σε uint8 τύπο data, και τελική εικόνα με blocks
+
+cv2.imshow('lenna image', lenna_image)
+cv2.imshow('Blocks after 2D-DCT', final_blocks_image)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()

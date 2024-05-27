aac02257e229788785f4546438043b0123bb86ea
mparakouu <geobarakou7@gmail.com>
2024-05-27 16:06:52 +0300

    E13 ok

--- E13.py
+++ E13.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-# εικόνα 
+# board_image
 board_image = cv2.imread('board.png')
 
 signal_board_image = np.mean(board_image ** 2) # σήμα εικόνας
@@ -13,14 +13,38 @@ white_Gaussian_noise = np.random.normal(0, np.sqrt(variance_noise), board_image.
 noise_board_image = cv2.add(board_image, white_Gaussian_noise) # θόρυβο --> εικόνα
 
 # φίλτρο κινουμένου μέσου (moving average filter) & φίλτρο μεσαίου (median filter)
-kernel_size = 3  # kernel μέγεθος
-moving_average_filter = cv2.blur(noise_board_image, (kernel_size, kernel_size))
-median_filter = cv2.medianBlur(noise_board_image, kernel_size)
+filter_size = 3 # διαστάσεις  # 3x3 ή 9x9
+filter_size1 = 9 
+#filter_size2 = 32
 
-cv2.imshow('Board Image', board_image)
-cv2.imshow('White Gaussian Noise Image', noise_board_image)
-cv2.imshow('moving average filter to remove noise', moving_average_filter)
-cv2.imshow('median filter to remove noise', median_filter)
+moving_average_filter = cv2.blur(noise_board_image, (filter_size, filter_size)) 
+median_filter = cv2.medianBlur(noise_board_image, filter_size)
+
+
+                            # ΕΙΣΑΓΩΓΗ ΚΡΟΥΣΤΙΚΟΥ ΘΟΡΥΒΟΥ
+# κρουστικός θόρυβος με πιθανότητα : 30%
+kernel_noise = np.zeros(board_image.shape[:2], dtype=np.uint8)
+cv2.randu(kernel_noise, 0, 100)  # με random εισαγωγή του κρουστικού , RGB
+kernel_noise[kernel_noise < 30] = 255 # 30/100 ασπρα
+kernel_noise[kernel_noise < 255] = 0 # υπόλοιπα μαύρα
+
+# κρουστικός θόρυβος --> board_image
+noise_mask = cv2.merge([kernel_noise] * 3)  # Δημιουργία μάσκας θορύβου
+noisy_board_image1 = cv2.add(board_image, noise_mask)
+
+# φίλτρο κινουμένου μέσου (moving average filter) & φίλτρο μεσαίου (median filter)
+moving_average_filter1 = cv2.blur(noisy_board_image1, (filter_size, filter_size))
+median_filter1 = cv2.medianBlur(noisy_board_image1, filter_size)
+
+
+#cv2.imshow('Board Image', board_image)
+#cv2.imshow('White Gaussian Noise Image', noise_board_image)
+cv2.imshow('moving average filter to remove white gaussian noise', moving_average_filter)
+cv2.imshow('median filter to remove white gaussian noise', median_filter)
+
+cv2.imshow('moving average filter to remove impulse noise', moving_average_filter1)
+cv2.imshow('Median Filter to remove impulse noise', median_filter1)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 

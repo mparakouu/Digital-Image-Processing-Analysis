ae9aebf4a18b09fc02ba19b7ab86c126c8c8ec7c
mparakouu <geobarakou7@gmail.com>
2024-05-28 17:24:57 +0300

    E15 β

--- E15.py
+++ E15.py
@@ -8,27 +8,46 @@ newYork_image = cv2.imread('new_york.png')
 # BGR --> RGB 
 newYork_image_rgb = cv2.cvtColor(newYork_image, cv2.COLOR_BGR2RGB)
 standard_deviation = 1.9  # τυπική απόκλιση από [1.5, 2.0]
-# Φιλτράρισμα με 2D πυρήνα εξομάλυνσης Gauss
+# 2D πυρήνα εξομάλυνσης Gauss filter
 final_blurred_image = cv2.GaussianBlur(newYork_image_rgb, (0, 0), standard_deviation)
 
-# ΠΡΟΣΘΗΚΗ ΛΕΥΚΟΥ ΘΟΡΥΒΟΥ GAUSSIAN
+
+
+                        # ΠΡΟΣΘΗΚΗ ΛΕΥΚΟΥ ΘΟΡΥΒΟΥ GAUSSIAN
 power = np.mean(final_blurred_image ** 2)  # ενέργεια σήματος
 variance_noise = power / (10 ** (7 / 10))  # διασπορά θορύβου
 white_Gaussian_noise = np.random.normal(0, np.sqrt(variance_noise), final_blurred_image.shape).astype(np.uint8)
 noisy_image = cv2.add(final_blurred_image, white_Gaussian_noise)  # white gaussian noise add --> final_blurred_image
 
-# Εξασφάλιση ότι οι τιμές των pixel παραμένουν στο επιτρεπτό εύρος [0, 255]
+# οι τιμές των pixel --> εύρος [0, 255]
 noisy_image = np.clip(noisy_image, 0, 255).astype(np.uint8)
 
-                        # ΚΡΟΥΣΤΙΚΗ ΑΠΟΚΡΙΣΗ & ΑΠΟΚΡΙΣΗ ΣΥΧΝΟΤΗΤΑΣ ΤΟΥ ΦΙΛΤΡΟΥ ΥΠΟΒΑΘΜΙΣΗΣ 
+
+
+                    # ΚΡΟΥΣΤΙΚΗ ΑΠΟΚΡΙΣΗ & ΑΠΟΚΡΙΣΗ ΣΥΧΝΟΤΗΤΑΣ ΤΟΥ ΦΙΛΤΡΟΥ ΥΠΟΒΑΘΜΙΣΗΣ 
 kernel_size = 9  # πυρήνα φίλτρου υποβάθμισης (9 x 9)
 kernel = np.ones((kernel_size, kernel_size), np.float32) / (kernel_size * kernel_size)
 
 
 fourier_kernel = np.fft.fft2(kernel, s=(256, 256)) # κρουστική απόκριση --> # fourier στον πυρήνα
-fshift_kernel = np.fft.fftshift(fourier_kernel)  # μετατόπιση του μετασχ. πυρήνα στο κέντρο 
-Frequency_response_kernel = 20 * np.log(np.abs(fshift_kernel) + 1e-10) # απόκριση συχνότητας 
+fourier_shift_kernel = np.fft.fftshift(fourier_kernel)  # μετατόπιση του μετασχ. πυρήνα στο κέντρο 
+Frequency_response_kernel = 20 * np.log(np.abs(fourier_shift_kernel) + 1e-10) # απόκριση συχνότητας 
 
+
+                    # WIENER DECONVOLUTION
+
+SNR = 1 / variance_noise 
+
+
+# Υπολογισμός του Wiener Filter
+wiener_filter = np.ones_like(fourier_shift_kernel, dtype=np.complex128)
+
+wiener_filter[:256, :256] = np.conj(fourier_shift_kernel) / (np.abs(fourier_shift_kernel) ** 2 + 1 / SNR)
+
+
+# Εφαρμογή Wiener Deconvolution
+restored_image = np.fft.ifft2(np.fft.fft2(noisy_image) * wiener_filter).real
+restored_image = np.clip(restored_image, 0, 255).astype(np.uint8)
 
 
 cv2.imshow('New York image', newYork_image_rgb)
@@ -50,11 +69,14 @@ plt.title('Frequency response')
 plt.axis('off')
 
 plt.subplot(121)
-plt.imshow(np.real(fshift_kernel), cmap='gray', interpolation='none')
+plt.imshow(np.real(fourier_shift_kernel), cmap='gray', interpolation='none')
 plt.title('fourier transf. kernel in the center')
 plt.axis('off')
 
 plt.show()
+
+# Παρουσίαση της αποκατεστημένης εικόνας
+cv2.imshow('Restored Image using Wiener Deconvolution', restored_image)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()

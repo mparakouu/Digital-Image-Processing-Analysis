94b2e95b611f3911fe454a7a16859535311ba87c
mparakouu <geobarakou7@gmail.com>
2024-05-29 12:56:52 +0300

    E16 OK

--- E16.py
+++ E16.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 
 # hallway image , γκρι 
+hallway = cv2.imread('hallway.png')
 hallway_image = cv2.imread('hallway.png', cv2.IMREAD_GRAYSCALE)
 
 x_sobel = cv2.Sobel(hallway_image, cv2.CV_64F, 1, 0, ksize=3) # sobel στον x
@@ -13,6 +14,7 @@ y_sobel = cv2.Sobel(hallway_image, cv2.CV_64F, 0, 1, ksize=3) # sobel στον y
 absolute_y_sobel = np.abs(y_sobel)
 
 image_gradient = np.sqrt(x_sobel**2 + x_sobel**2) # image's gradient
+
 
 
                     # ΜΕΤΑΣΧΗΜΑΤΙΣΜΟΣ ΦΩΤΕΙΝΟΤΗΤΑΣ --> GAMMA CORRECTION
@@ -25,10 +27,20 @@ gamma = 0.3
 image_gradient_gamma = gamma_correction(image_gradient, gamma)
 
 
+
                    # ΟΛΙΚΗ ΚΑΤΩΦΛΙΩΣΗ ΣΤΗΝ ΕΙΚΟΝΑ ΠΛΑΤΟΥΣ ΤΟΥ GRADIENT
-threshold = 0.5  # τιμή κατωφλίου
+threshold = 1  # τιμή κατωφλίου
 _, thresholded_image = cv2.threshold(image_gradient_gamma, threshold / 255, 1, cv2.THRESH_BINARY)
 
+
+
+                   # ΜΕΤΑΣΧΗΜΑΤΙΣΜΟΣ HOUGH
+Hough_lines = cv2.HoughLinesP(
+    image_gradient.astype(np.uint8),  rho=1, theta=np.pi/180, threshold=5,  minLineLength=1, maxLineGap=5
+)
+for line in Hough_lines:
+    x1, y1, x2, y2 = line[0]
+    cv2.line(hallway, (x1, y1), (x2, y2), (255, 0, 255), 2)
 
 
 fig, axs = plt.subplots(1, 4, figsize=(24, 6))
@@ -48,15 +60,21 @@ axs[2].set_title('Gradient (gamma correction)')
 axs[2].axis('off')
 
 axs[3].hist(image_gradient_gamma.ravel(), bins=256, range=[0, 1], color='black')
-axs[3].axvline(x=threshold, color='red', linestyle='--')
-axs[3].set_title('Histogram with threshold')
+axs[3].axvline(x=threshold, color='purple', linestyle='--')
+axs[3].set_title('Histogram')
 axs[3].set_xlabel('Pixel intensity')
 axs[3].set_ylabel('Frequency')
 
-plt.show()
 
 plt.figure(figsize=(6, 6))
 plt.imshow(thresholded_image, cmap='gray')
 plt.title('Gradient image with threshold')
 plt.axis('off')
+
+
+plt.figure(figsize=(8, 4))
+plt.imshow(hallway, cmap='gray')
+plt.title('Hough algorithm')
+plt.axis('off')
+
 plt.show()

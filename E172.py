fe30af6931c283a1b564e74cad79f2fc4cecb45a
mparakouu <geobarakou7@gmail.com>
2024-05-30 14:33:13 +0300

    Ε17 2 half ok

--- E172.py
+++ E172.py
@@ -1,23 +1,39 @@
 import cv2
 import numpy as np
 
+# football_image
 football_image = cv2.imread('football.jpg')
 
+# 2D pixel array και --> float 32
+pixels = football_image.reshape((-1, 3)).astype(np.float32)
 
-pixels = football_image.reshape((-1, 3)).astype(np.float32) # RGB για κάθε pixels
+# k-means
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
 
+# k-means --> k = 2 , 3 , 4
+k_values = [2, 3, 4]
 
-k = 3  # αριθμός των clusters
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2) # κριτήρια για τον k-means & αριθμό loops
-_, labels, centers = cv2.kmeans(pixels, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
-centers = np.uint8(centers)
+for k in k_values:
+    _, labels, centers = cv2.kmeans(pixels, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+    centers = np.uint8(centers) #  κέντρα σημείων --> unit8
 
+    black_background = np.zeros_like(football_image)
 
-segmented_image = centers[labels.flatten()]
-segmented_image = segmented_image.reshape(football_image.shape)
+    for i in range(k):
+
+        # τα pixel της εικόνας που θα εμφανιστού με την πράξη --> AND.
+        # εάν pixel στην mask = 1 --> ίδιο στην εικόνα
+        # εάν pixel στην mask = 0 --> μαύρο στην εικόνα 
+        mask = np.uint8(labels == i)
+        mask_resized = cv2.resize(mask, (football_image.shape[1], football_image.shape[0])) 
+        mask_resized = mask_resized.reshape(football_image.shape[0], football_image.shape[1], -1)
+        mask_resized = np.uint8(mask_resized)
+        
+        segment = cv2.bitwise_and(football_image, football_image, mask=mask_resized)
+        cv2.imshow(f'Segment {i+1} with k={k}', segment)
 
 
 cv2.imshow('football image', football_image)
-cv2.imshow('Segmented Image', segmented_image)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
